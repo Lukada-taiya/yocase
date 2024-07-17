b68823a722c038cfceec80d683b06ca54f62caab
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-17 12:22:05 +0000

    Feat: Added Providers implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 // import Footer from '@/components/Footer'
 // import { Toaster } from '@/components/ui/toaster'
-// import Providers from '@/components/Providers'
+import Providers from '@/components/Providers';
 // import { constructMetadata } from '@/lib/utils'
 
 const recursive = Recursive({ subsets: ['latin'] })
@@ -21,14 +21,14 @@ export default function RootLayout({
       <body className={recursive.className}>
         <Navbar />
 
-        {/* <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
           <div className='flex-1 flex flex-col h-full'>
             <Providers>{children}</Providers>
           </div>
-          <Footer />
+          {/* <Footer /> */}
         </main>
 
-        <Toaster /> */}
+        {/* <Toaster /> */}
       </body>
     </html>
   )
